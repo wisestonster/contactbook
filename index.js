12088ea246e3af213023190ca620f56db76a091b
wisestonster <wisestonster@gmail.com>
2020-12-14 10:08:38 +0900

    index and create

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser'); 
 var app = express();
 
 // DB setting
@@ -22,9 +22,43 @@ db.on('error', function (err) {
     console.log('DB ERROR : ', err);
 });
 
-// View Engine setting
+// Other settings
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json()); // 2
+app.use(bodyParser.urlencoded({extended:true})); // 3
+
+// DB schema // 4
+var contactSchema = mongoose.Schema({
+    name:{type:String, required:true, unique:true},
+    email:{type:String},
+    phone:{type:String}
+  });
+var Contact = mongoose.model('contact', contactSchema); // 5
+
+// Routes
+// Home // 6
+app.get('/', function(req, res){
+    res.redirect('/contacts');
+  });
+  // Contacts - Index // 7
+  app.get('/contacts', function(req, res){
+    Contact.find({}, function(err, contacts){
+      if(err) return res.json(err);
+      res.render('contacts/index', {contacts:contacts});
+    });
+  });
+  // Contacts - New // 8
+  app.get('/contacts/new', function(req, res){
+    res.render('contacts/new');
+  });
+  // Contacts - create // 9
+  app.post('/contacts', function(req, res){
+    Contact.create(req.body, function(err, contact){
+      if(err) return res.json(err);
+      res.redirect('/contacts');
+    });
+  });
 
 // Port setting
 var port = 3000;
